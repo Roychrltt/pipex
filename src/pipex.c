dd3e7d63c0c1fb835e3227bd9fcea3db529fcff1
Charlotte Roy <roychrltt@chacha.home>
2024-06-10 10:03:53 +0200

    just do it

--- src/pipex.c
+++ src/pipex.c
@@ -6,22 +6,43 @@
 /*   By: xiaxu <xiaxu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/08 14:28:31 by xiaxu             #+#    #+#             */
-/*   Updated: 2024/06/09 18:03:13 by roychrltt        ###   ########.fr       */
+/*   Updated: 2024/06/10 10:03:34 by roychrltt        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-static void	exit_handler(void)
+void	child(char **argv, char **envp, int *fd_tab)
 {
-	ft_putstr_fd("Usage: ./pipex <infile> <cmd1> <cmd2> <outfile>\n", 2);
-	exit(EXIT_FAILURE);
+	int	fd;
+
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+
 }
 
 int	main(int argc, char **argv, char **envp)
 {
-	(void) argv;
-	(void) envp;
+	int		fd_tab[2];
+	pid_t	pid;
+
 	if (argc != 5)
-		exit_handler();
+	{
+		ft_putstr_fd("Usage: ./pipex <infile> <cmd1> <cmd2> <outfile>\n", 2);
+		exit(EXIT_FAILURE);
+	}
+	if (pipe(fd_tab) == -1)
+	{
+		perror("pipe");
+		exit(EXIT_FAILURE);
+	}
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
+		child(argv, fd_tab, envp);
+	parent(argv, fd_tab, envp);
 }
